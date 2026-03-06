7480d2e651e8dba963c3b046758c5a1edbb38e03
maherraz <maherraz@student.42.mg>
2026-03-06 07:52:12 +0300

    feat: add ft_printf.c
    - Implemented core logic for ft_printf.c
    - Norminette compliant
    - Basic tests passed

--- ft_printf.c
+++ ft_printf.c
@@ -1,62 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maherraz <maherraz@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 10:31:42 by maherraz          #+#    #+#             */
-/*   Updated: 2026/03/02 10:46:32 by maherraz         ###   ########.fr       */
+/*   Updated: 2026/03/05 07:38:36 by maherraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_check_list(va_list args, char c)
+static int	ft_manage_conversion(va_list args, char c)
 {
 	if (c == 'c')
 		return (ft_putchar(va_arg(args, int)));
 	if (c == 's')
 		return (ft_putstr(va_arg(args, char *)));
 	if (c == 'p')
-		return (ft_print_ptr(va_arg(args, void *)));
+		return (ft_print_pointer(va_arg(args, void *)));
 	if (c == 'd' || c == 'i')
 		return (ft_putnbr(va_arg(args, int)));
 	if (c == 'u')
 		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
 	if (c == 'x')
-		return (ft_puthexa_min(va_arg(args, unsigned int)));
+		return (ft_print_hex(va_arg(args, unsigned int), 0));
 	if (c == 'X')
-		return (ft_puthexa_maj(va_arg(args, unsigned int)));
+		return (ft_print_hex(va_arg(args, unsigned int), 1));
 	if (c == '%')
 		return (ft_putchar('%'));
-	return (0);
+	return (write(1, "%", 1) + write(1, &c, 1));
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
-	int		count;
+	int		i;
 
 	if (!format)
-		return (-1);
+		return (0);
 	va_start(args, format);
-	count = 0;
+	i = 0;
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format)
-				count += ft_check_list(args, *format);
-			format++;
+			i += ft_manage_conversion(args, *format);
 		}
 		else
-		{
-			count += ft_putchar(*format);
-			format++;
-		}
+			i += ft_putchar(*format);
+		format++;
 	}
 	va_end(args);
-	return (count);
+	return (i);
 }
